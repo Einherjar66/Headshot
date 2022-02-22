16742f07fbbb87a81ec161889bf64795d41a8314
Daniel Soler <danielsoler72@yahoo.de>
2022-02-22 22:20:05 +0100

    Added ShooterCharacter AnimInstance

---
+++ Source/Headshot/ShooterAnimInstance.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAnimInstance.h"
+
+#include "ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (ShooterCharacter == nullptr) // Wenn der Shooter Character null ist.
+	{ 
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());							// Dann versuch die AnimInstance wieder zurückzuholen.
+	}
+
+	if (ShooterCharacter)
+	{
+		// Get the lateral speed of the character from velocity
+		FVector Velocity{ ShooterCharacter->GetVelocity() };
+		Velocity.Z = 0.f;
+		Speed = Velocity.Size();
+
+		// Is the character in the air?
+		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();						// Checkt ob der Character am fallen ist oder nicht
+
+		// Is the Char accelerating ?
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f)	// Checkt ob der Char "Beschleunigt"
+		{
+			bIsAccelerating = true;
+		}
+		else
+		{
+			bIsAccelerating = false;
+		}
+	}
+}
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());								// Gibt uns die AnimInstance vom Pawn
+}
