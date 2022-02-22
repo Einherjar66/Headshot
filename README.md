0025bce6720466f59cfeb0f4650d5a67cbd35068
Daniel Soler <danielsoler72@yahoo.de>
2022-02-22 01:40:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Headshot
+
+Developed with Unreal Engine 4
---
+++ Source/Headshot/HeadshotGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HeadshotGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEADSHOT_API AHeadshotGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
